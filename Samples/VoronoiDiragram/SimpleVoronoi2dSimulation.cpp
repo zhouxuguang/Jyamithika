a9dc7d1238432ea5cc79efd153a69486a5eeacbd
zhouxuguang <zhouxuguang236@126.com>
2023-11-12 16:36:05 +0800

    增加mapbox的d网三角剖分

--- Samples/VoronoiDiragram/SimpleVoronoi2dSimulation.cpp
+++ Samples/VoronoiDiragram/SimpleVoronoi2dSimulation.cpp
@@ -23,6 +23,7 @@
 #include "Jyamithika/Core/Primitives/Point.h"
 #include "Jyamithika/Core/Primitives/Bounds.h"
 #include "Jyamithika/Voronoi.h"
+#include "Jyamithika/delaunator.h"
 
 #include <algorithm>
 #include <chrono>
@@ -77,6 +78,28 @@ void setup_pointcloud(std::vector<jmk::Point2d>& points)
 	std::unique(points.begin(), points.end());
 }
 
+int testDelaunator()
+{
+    /* x0, y0, x1, y1, ... */
+    std::vector<double> coords = {-1, 1, 1, 1, 1, -1, -1, -1};
+
+    //triangulation happens here
+    delaunator::Delaunator d(coords);
+
+    for(std::size_t i = 0; i < d.triangles.size(); i+=3)
+    {
+        printf(
+            "Triangle points: [[%f, %f], [%f, %f], [%f, %f]]\n",
+            d.coords[2 * d.triangles[i]],        //tx0
+            d.coords[2 * d.triangles[i] + 1],    //ty0
+            d.coords[2 * d.triangles[i + 1]],    //tx1
+            d.coords[2 * d.triangles[i + 1] + 1],//ty1
+            d.coords[2 * d.triangles[i + 2]],    //tx2
+            d.coords[2 * d.triangles[i + 2] + 1] //ty2
+        );
+    }
+}
+
 
 int main(void)
 {
@@ -127,6 +150,9 @@ int main(void)
 		ImGui_ImplOpenGL3_Init("#version 330");
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 	}
+    
+    //测试mapbox算法
+    testDelaunator();
 
 	std::vector<jmk::Point2d> points;
 	std::vector<float> point_data;
